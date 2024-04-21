24ac8ccd2b01118e52ad94824e748b4e83c7fb0b
PetroHryb <ipz234_gpyu@student.ztu.edu.ua>
2024-04-21 17:18:33 +0300

    Complete small task

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sana Commerce Software Development Courses `24
+![Sana Commerce](https://upload.wikimedia.org/wikipedia/commons/0/08/Sana_Commerce_Logo.png)
+
+Виконав: ***Гриб Петро Юрійович***
+ipz234_gpyu@student.ztu.edu.ua
---
+++ Sana10/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Sana10.Models;
+using System.Diagnostics;
+
+namespace Sana10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Welcome(string userName = "No Name")
+        {
+            ViewBag.Message = userName;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
